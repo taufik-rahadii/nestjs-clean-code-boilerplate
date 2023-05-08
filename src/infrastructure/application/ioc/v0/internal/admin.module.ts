113f1dae3c8b3bb26b2a580e134f9b7451a0da52
Taufik Rahadi <taufik.rahadi@akarinti.tech>
2023-05-08 08:08:01 +0700

    refactor: using controller and remove resolver

--- src/infrastructure/application/ioc/v0/internal/admin.module.ts
+++ src/infrastructure/application/ioc/v0/internal/admin.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { AdminResolver } from '../../../presenters/v0/admin/admin.resolver'
 import { Admin } from '../../../use_cases/admin/admin.entity'
 import { AdminService } from '../../../use_cases/admin/admin.service'
 import { AdminRepository } from '../../../use_cases/admin/admin.repository'
-import { AdminController } from 'src/infrastructure/application/presenters/v0/admin/admin.controller'
+import { AdminController } from '../../../presenters/v0/admin/admin.controller'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Admin])],
   controllers: [AdminController],
   providers: [
-    AdminResolver,
     {
       provide: 'ADMIN_SERVICE',
       useClass: AdminService,
@@ -20,6 +18,5 @@ import { AdminController } from 'src/infrastructure/application/presenters/v0/ad
       useClass: AdminRepository,
     },
   ],
-  exports: [AdminResolver],
 })
 export class AdminModule {}
